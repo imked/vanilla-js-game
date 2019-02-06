9287a4a5cb4fb8b93e9f5172f35039f0897fdcc0
imked <imke.duensing@gmail.com>
2019-02-06 15:41:07 +0100

    Update game

---
+++ js/Bird.js
@@ -0,0 +1,43 @@
+import { throws } from 'assert'
+
+export default class Bird {
+  defaultConfig = {
+    color: 'black',
+    speed: 1 + Math.random() * 8,
+    position: 0,
+  }
+
+  constructor(config) {
+    config = { ...this.defaultConfig, ...config }
+    const { color, speed, position, removeBird } = config
+
+    this.color = color
+    this.position = position
+    this.removeBird = removeBird
+    this.speed = speed
+    this.el = this.render()
+    this.addClickHandler()
+  }
+
+  addClickHandler() {
+    this.el.addEventListener('click', () => console.log('bird was shot'))
+  }
+
+  update() {
+    this.position = this.position + this.speed
+    if (this.position > window.innerWidth) {
+      this.removeBird(this)
+      this.el.remove()
+    } else {
+      this.el.style.left = this.position + 'px'
+    }
+  }
+
+  render() {
+    const el = document.createElement('div')
+    el.className = 'bird'
+    el.style.background = this.color
+    document.body.insertAdjacentElement('beforeend', el)
+    return el
+  }
+}
---
+++ js/Game.js
@@ -0,0 +1,37 @@
+import Bird from './Bird'
+
+export default class Game {
+  birds = []
+
+  constructor() {
+    this.createBirds()
+    this.loop()
+  }
+
+  createBirds() {
+    this.addBird()
+    this.addBird()
+    this.addBird()
+    this.addBird()
+  }
+
+  addBird() {
+    const config = {
+      removeBird: this.removeBird,
+    }
+
+    this.birds = [...this.birds, new Bird(config)]
+  }
+  //removes Bird from loop
+  removeBird = bird => {
+    const index = this.birds.indexOf(bird)
+    this.birds = [...this.birds.slice(0, index), ...this.birds.slice(index + 1)]
+  }
+
+  loop() {
+    //this.counter++ % 60 === && this.addBird()
+    Math.random() < 1 / 60 && this.addBird()
+    this.birds.forEach(bird => bird.update())
+    requestAnimationFrame(() => this.loop())
+  }
+}
